18ce07f349798d87e284fda355a9c2bf78b4c858
1Step621 <onestep621@gmail.com>
2026-01-02 03:42:56 +0900

    wip: iranai?

--- app.config.ts
+++ app.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: {
-    preset: "cloudflare-pages",
-    rollupConfig: {
-      external: ["node:async_hooks"],
-    },
-  },
   solid: {
     babel: {
       compact: false,
